d521ca78837a26b4319f391151977347ba3ef9b2
Hoang Anh <anhnh265@gmail.com>
2024-06-25 15:25:41 +0700

    video 30

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -1,6 +1,16 @@
+
+
+import './style.css';
 const MyComponent = () =>{
   return(
+    <>
     <div>Hoang Anh & React JS</div>
+    <div 
+    style={{
+      borderRadius:"10px"
+    }}
+    className="child">child</div>
+    </>
   ) ;
 }
 export default MyComponent;
