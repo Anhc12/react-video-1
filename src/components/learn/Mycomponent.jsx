871551ac69db1fa309ac964afe0813412412054b
Hoang Anh <anhnh265@gmail.com>
2024-06-25 16:04:19 +0700

    video 31

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -2,13 +2,24 @@
 
 import './style.css';
 const MyComponent = () =>{
+  // const anhc = "hoang anh1";//string ok
+  // const anhc = "30";//number ok
+  // const anhc = "true";//boolean no
+  // const anhc = "underfined";//no
+  // const anhc = null;//no
+  const anhc = [1, 2, 3]
+  // const anhc = {
+  //   name: "anhc",
+  //   age: 30
+  // }
   return(
     <>
-    <div>Hoang Anh & React JS</div>
+    <div>{JSON.stringify(anhc)} & React JS</div>
+    <div>{console.log("anhc")}</div>
     <div 
-    style={{
-      borderRadius:"10px"
-    }}
+    style={
+      {borderRadius:"10px"}
+    }
     className="child">child</div>
     </>
   ) ;
