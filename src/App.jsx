bd2171395eadd0a5b6bfd0f075cfff15592529d3
Hoang Anh <anhnh265@gmail.com>
2024-06-28 19:25:09 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,23 @@ import reactLogo from './assets/react.svg'
 
 const App= () => {
   
- 
+  const anhc = " hoang anh Cityzen";
+  const age = 25;
+  const data ={address: "hanoi",
+    country : "vietNam"
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData 
+      
+
+      name = {anhc}
+      age = {age}
+      data = {data}
+      
+      />
       <div className='todo-image'>
         <img src={reactLogo} className='logo'/>
 
