1649f0f2c07c434155b78c91899c2b42908414b9
Hoang Anh <anhnh265@gmail.com>
2024-07-09 01:57:36 +0700

    video 38

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,18 @@ const App= () => {
   }
 
   const addNewTodo = (name) =>{
-    // alert (`call me ${name}`)
+    const newTodo = {
+      id: randomIntFromInterval(1,1000000),
+      name: name
+    }
+    setTodoList([...todoList, newTodo])
+
   }
-  addNewTodo();
+  const randomIntFromInterval=(min, max)=> { // min and max included
+return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+  // addNewTodo();
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
