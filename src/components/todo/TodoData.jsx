f19412607aeccccf560b815f3cc355f5376403a8
Hoang Anh <anhnh265@gmail.com>
2024-07-09 18:29:02 +0700

    video 39

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,12 +1,19 @@
 const TodoData = (props) => {
 
-  const {name, age, data} = props;
-console.log(".>>>> check props ", props)
+  const {todoList} = props;
+console.log(".>>>> check props ", todoList)
   return(
     <div className='todo-data'>
-      <div>My name is :{name}</div>
-        <div>Learning React</div>
-        <div>Watching Youtube</div>
+      {todoList.map((item, index)=>{
+        console.log(">>> check map: ", item, index )
+        return(
+        <div className="todo-item">
+          <div>{item.name}</div>
+          
+          <button>Delete</button>
+        </div>)
+      })}
+        
         <div>{JSON.stringify(props.todoList)}</div>
       </div>
   )
