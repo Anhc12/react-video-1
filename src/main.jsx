dff7e027640f41411035a7cf901e5f6aca1e6476
Hoang Anh <anhnh265@gmail.com>
2024-08-07 02:08:33 +0700

    video 46

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import {
   createBrowserRouter,
   RouterProvider,
   } from "react-router-dom";
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import UserPage from './pages/user.jsx';
+import ProductsPage from './pages/prooduct.jsx';
+import './styles/global.css'
   const router = createBrowserRouter([
     {
     path: "/",
@@ -12,16 +17,20 @@ import {
     },
     {
       path: "/login",
-      element: <div>login page</div>,
+      element: <LoginPage/>,
     },
     {
       path: "/register",
-      element: <div>register page</div>,
+      element: <RegisterPage/>
     },
     {
       path: "/users",
-      element: <div>users page</div>,
+      element: <UserPage/>
     },
+    {
+      path: "/products",
+      element: <ProductsPage/>
+    }
     ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
